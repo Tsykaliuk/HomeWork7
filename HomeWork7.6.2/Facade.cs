5daf8f0649c897116af1fa34b1ed48fd79d76218
Tsykaliuk Maksym <tsykaliuk.maksym@gmail.com>
2024-11-12 17:33:08 +0200

    renamed folders

--- HomeWork7.6.2/Facade.cs
+++ HomeWork7.6.2/Facade.cs
@@ -1,73 +1,77 @@
 ﻿using System;
 
-class Amplifier
-{
-    public void On() => Console.WriteLine("Amplifier is on.");
-    public void SetVolume(int volumeLevel) => Console.WriteLine($"Amplifier volume set to {volumeLevel}.");
-    public void Off() => Console.WriteLine("Amplifier is off.");
-}
 
-class DvdPlayer
-{
-    public void On() => Console.WriteLine("DVD Player is on.");
-    public void Play(string movieName) => Console.WriteLine($"Playing {movieName}.");
-    public void Stop() => Console.WriteLine("DVD Player stopped.");
-    public void Off() => Console.WriteLine("DVD Player is off.");
-}
-
-class Projector
-{
-    public void On() => Console.WriteLine("Projector is on.");
-    public void SetWideScreenMode() => Console.WriteLine("Projector in widescreen mode.");
-    public void Off() => Console.WriteLine("Projector is off.");
-}
-
-class HomeTheaterFacade
-{
-    private Amplifier amplifier;
-    private DvdPlayer dvdPlayer;
-    private Projector projector;
-
-    public HomeTheaterFacade(Amplifier amplifier, DvdPlayer dvdPlayer, Projector projector)
+namespace Facade
+{ 
+    class Amplifier
     {
-        this.amplifier = amplifier;
-        this.dvdPlayer = dvdPlayer;
-        this.projector = projector;
+        public void On() => Console.WriteLine("Amplifier is on.");
+        public void SetVolume(int volumeLevel) => Console.WriteLine($"Amplifier volume set to {volumeLevel}.");
+        public void Off() => Console.WriteLine("Amplifier is off.");
     }
 
-    public void WatchMovie(string movie)
+    class DvdPlayer
     {
-        Console.WriteLine("Get ready to watch a movie...");
-        amplifier.On();
-        amplifier.SetVolume(5);
-        dvdPlayer.On();
-        dvdPlayer.Play(movie);
-        projector.On();
-        projector.SetWideScreenMode();
+        public void On() => Console.WriteLine("DVD Player is on.");
+        public void Play(string movieName) => Console.WriteLine($"Playing {movieName}.");
+        public void Stop() => Console.WriteLine("DVD Player stopped.");
+        public void Off() => Console.WriteLine("DVD Player is off.");
     }
 
-    public void EndMovie()
+    class Projector
     {
-        Console.WriteLine("Shutting down the movie theater...");
-        dvdPlayer.Stop();
-        dvdPlayer.Off();
-        amplifier.Off();
-        projector.Off();
+        public void On() => Console.WriteLine("Projector is on.");
+        public void SetWideScreenMode() => Console.WriteLine("Projector in widescreen mode.");
+        public void Off() => Console.WriteLine("Projector is off.");
     }
-}
 
-class Program
-{
-    static void Main()
+    class HomeTheaterFacade
     {
-        Amplifier amplifier = new Amplifier();
-        DvdPlayer dvdPlayer = new DvdPlayer();
-        Projector projector = new Projector();
+        private Amplifier amplifier;
+        private DvdPlayer dvdPlayer;
+        private Projector projector;
 
-        HomeTheaterFacade homeTheater = new HomeTheaterFacade(amplifier, dvdPlayer, projector);
+        public HomeTheaterFacade(Amplifier amplifier, DvdPlayer dvdPlayer, Projector projector)
+        {
+            this.amplifier = amplifier;
+            this.dvdPlayer = dvdPlayer;
+            this.projector = projector;
+        }
 
-        homeTheater.WatchMovie("Inception");
-        Console.WriteLine();
-        homeTheater.EndMovie();
+        public void WatchMovie(string movie)
+        {
+            Console.WriteLine("Get ready to watch a movie...");
+            amplifier.On();
+            amplifier.SetVolume(5);
+            dvdPlayer.On();
+            dvdPlayer.Play(movie);
+            projector.On();
+            projector.SetWideScreenMode();
+        }
+
+        public void EndMovie()
+        {
+            Console.WriteLine("Shutting down the movie theater...");
+            dvdPlayer.Stop();
+            dvdPlayer.Off();
+            amplifier.Off();
+            projector.Off();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Amplifier amplifier = new Amplifier();
+            DvdPlayer dvdPlayer = new DvdPlayer();
+            Projector projector = new Projector();
+
+            HomeTheaterFacade homeTheater = new HomeTheaterFacade(amplifier, dvdPlayer, projector);
+
+            homeTheater.WatchMovie("Inception");
+            Console.WriteLine();
+            homeTheater.EndMovie();
+        }
     }
 }
