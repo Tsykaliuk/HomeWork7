88309fe56a6d813823795ec5eb2f065da093dc0f
Tsykaliuk Maksym <tsykaliuk.maksym@gmail.com>
2024-11-12 17:13:52 +0200

    Add project files.

---
+++ HomeWork7.4/Decorator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+namespace Decorator.Examples
+{
+    class MainApp
+    {
+        static void Main()
+        {
+            // Create ConcreteComponent and two Decorators
+            ChristmasTree christmasTree = new ChristmasTree();
+            OrnamentDecorator ornaments = new OrnamentDecorator();
+            GarlandDecorator garland = new GarlandDecorator();
+
+            // Link decorators
+            ornaments.SetTree(christmasTree);
+            garland.SetTree(ornaments);
+
+            garland.Display();
+
+            // Wait for user
+            Console.Read();
+        }
+    }
+    // "Component"
+    abstract class Tree
+    {
+        public abstract void Display();
+    }
+
+    // "ConcreteComponent"
+    class ChristmasTree : Tree
+    {
+        public override void Display()
+        {   
+            Console.WriteLine("Christmas Tree");
+        }
+    }
+    // "Decorator"
+    abstract class TreeDecorator : Tree
+    {
+        protected Tree tree;  
+
+        public void SetTree(Tree tree)
+        {
+            this.tree = tree;
+        }
+        public override void Display()
+        {
+            if (tree != null)
+            {
+                tree.Display();
+            }
+        }
+    }
+
+    // "ConcreteDecoratorA"
+    class OrnamentDecorator : TreeDecorator
+    {
+        private string ornaments;
+
+        public override void Display()
+        {
+            base.Display();
+            ornaments = "Ornaments";
+            Console.WriteLine("Decorated with " + ornaments);
+        }
+    }
+
+    // "ConcreteDecoratorB" 
+    class GarlandDecorator : TreeDecorator
+    {
+        public override void Display()
+        {
+            base.Display();
+            AddGarland();
+        }
+        void AddGarland()
+        {
+            Console.WriteLine("Garland lights are shining");
+        }
+    }
+}
---
+++ HomeWork7.6/FactoryMethod.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    // Product
+    public abstract class Message
+    {
+        public abstract void Send();
+    }
+
+    // Concrete Products
+    public class EmailMessage : Message
+    {
+        public override void Send() => Console.WriteLine("Sending Email Message...");
+    }
+
+    public class SMSMessage : Message
+    {
+        public override void Send() => Console.WriteLine("Sending SMS Message...");
+    }
+
+    public class ViberMessage : Message
+    {
+        public override void Send() => Console.WriteLine("Sending Viber Message...");
+    }
+
+    // Creator
+    public abstract class MessageFactory
+    {
+        public abstract Message CreateMessage();
+    }
+
+    // Concrete Creators
+    public class EmailFactory : MessageFactory
+    {
+        public override Message CreateMessage()
+        {
+            return new EmailMessage();
+        }
+    }
+
+    public class SMSFactory : MessageFactory
+    {
+        public override Message CreateMessage()
+        {
+            return new SMSMessage();
+        }
+    }
+
+    public class ViberFactory : MessageFactory
+    {
+        public override Message CreateMessage()
+        {
+            return new ViberMessage();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a factory for each message type
+            MessageFactory emailFactory = new EmailFactory();
+            MessageFactory smsFactory = new SMSFactory();
+            MessageFactory viberFactory = new ViberFactory();
+
+            // Create and send messages
+            Message email = emailFactory.CreateMessage();
+            email.Send();
+
+            Message sms = smsFactory.CreateMessage();
+            sms.Send();
+
+            Message push = viberFactory.CreateMessage();
+            push.Send();
+
+            Console.ReadKey();
+        }
+    }
+}
